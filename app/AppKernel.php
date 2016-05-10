83eba3568b116d2fd715a8c3caf47807ea54455f
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2016-05-10 21:24:06 +0100

    implemented jms/serializer-bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             new AppBundle\AppBundle(),
         ];
 
